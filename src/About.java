cfb73c13d2318a9932ba40763b04244af0a6b39f
Ibrahim Muhammad <22011598-116@uog.edu.pk>
2023-07-23 13:26:39 +0500

    Update About.java

--- src/About.java
+++ src/About.java
@@ -10,6 +10,7 @@ public class About extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        System.out.println("-------------------");
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
